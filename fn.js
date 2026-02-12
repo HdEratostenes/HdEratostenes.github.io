5e9efa6594b7a2a8f99032149636014e20b31ee4
HdEratostenes <h.de.eratostenes@proton.me>
2026-02-11 22:02:43 -0600

    Update fn.js

--- fn.js
+++ fn.js
@@ -3,9 +3,32 @@ function showPage(pageId){
         page.classList.remove('active');
     });
     document.getElementById(pageId).classList.add('active');
+    
 }
-
 function menufun(){
     const buttons = document.getElementById("textbuttons");
     buttons.classList.toggle("hidden");
+}
+function instagramImg(inst){
+    const img = document.querySelectorAll(
+        "#home #media #social #instagram #posts #latest #imgfeed #imgs img"
+    );
+    var len = img.length;
+    var ind = 0;
+    for(let i = 0; i < len; i++){
+        if(window.getComputedStyle(img[i]).display ==='block'){
+            ind = i;
+            break;
+        }
+        console.log(window.getComputedStyle(img[i]).display);
+    }
+    if(inst === 1 && ind !== len-1){
+        (img[ind+1]).style.display = 'block';
+        (img[ind]).style.display = 'none';
+        
+    }else if(inst === -1 && ind !== 0){
+        (img[ind-1]).style.display = 'block';
+        (img[ind]).style.display = 'none';
+
+    }
 }
