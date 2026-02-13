16e6099eb6464b50bbc0cc5452fe19048973d20f
HdEratostenes <h.de.eratostenes@proton.me>
2026-02-13 11:01:29 -0600

    pepito

--- fn.js
+++ fn.js
@@ -11,7 +11,30 @@ function menufun(){
 }
 function instagramImg(inst){
     const img = document.querySelectorAll(
-        "#home #media #social #instagram #posts #latest #imgfeed #imgs img"
+        "#imgs img"
+    );
+    var len = img.length;
+    var ind = 0;
+    for(let i = 0; i < len; i++){
+        if(window.getComputedStyle(img[i]).display ==='block'){
+            ind = i;
+            break;
+        }
+        console.log(window.getComputedStyle(img[i]).display);
+    }
+    if(inst === 1 && ind !== len-1){
+        (img[ind+1]).style.display = 'block';
+        (img[ind]).style.display = 'none';
+        
+    }else if(inst === -1 && ind !== 0){
+        (img[ind-1]).style.display = 'block';
+        (img[ind]).style.display = 'none';
+
+    }
+}
+function instagramImg2(inst){
+    const img = document.querySelectorAll(
+        "#imgs2 img"
     );
     var len = img.length;
     var ind = 0;
